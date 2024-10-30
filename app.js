6ef5c154908cb2af138c51f6fe91710452458f22
icet-sharadha <icet.sharadha@gmail.com>
2024-10-30 14:07:01 +0530

    load table

--- app.js
+++ app.js
@@ -51,79 +51,166 @@
 
 // Customer saman = new Customer("saman");
 
+// let customer = {
+//     name: "saman",
+//     age: 18,
+//     address: "panadura",
+//     salary: 7500.00
+// }
+
+// let customers = [
+//     {
+//         name: "saman",
+//         age: 20,
+//         address: "panadura"
+//     },
+//     {
+//         name: "nimal",
+//         age: "31",
+//         address: "kalutara"
+//     },
+//     {
+//         name: "sunil",
+//         age: 51,
+//         address: "moratuwa"
+//     }
+
+// ];
+
+// customers.push({
+//     name: "siripala",
+//     age: 78,
+//     address: "matugama"
+// })
+
+// customers.push({
+//     name: "siripala",
+//     age: 78,
+//     address: "matugama"
+// })
+
+
+// function loadTable(){
+//     let tblCustomerList =document.getElementById("tblCustomerList");
+//     let body=`
+//            <tr>
+//             <th>Name</th>
+//             <th>address</th>
+//             <th>age</th>
+//         </tr>`;
+    
+//     customers.forEach(customer =>{
+//         body+=`
+//         <tr>
+//             <td>${customer.name}</td>
+//             <td>${customer.address}</td>
+//             <td>${customer.age}</td>
+//         </tr>`;
+//     })
+//     tblCustomerList.innerHTML=body;
+//     console.log(body);
+    
+
+// }
+// loadTable();
+// function addCustomer() {
+//     let custName = document.getElementById("txtName").value;
+//     let custAddress = document.getElementById("txtAddress").value;
+//     let custAge = document.getElementById("txtAge").value;
+
+//     customers.push({
+//         name:custName,
+//         address:custAddress,
+//         age:custAge
+//     });
+//     loadTable();
+//     console.log(customers);
+// }
+
+// -------------------------------------------------------------------------------------
+
 let customer = {
-    name: "saman",
-    age: 18,
-    address: "panadura",
-    salary: 7500.00
+    name:"saman",
+    age:20,
+    address:"gampaha",
+    email:"saman@gmail.com"
 }
 
-let customers = [
-    {
-        name: "saman",
-        age: 20,
-        address: "panadura"
-    },
-    {
-        name: "nimal",
-        age: "31",
-        address: "kalutara"
-    },
-    {
-        name: "sunil",
-        age: 51,
-        address: "moratuwa"
-    }
+let customerList = [{
+    name:"saman",
+    age:20,
+    address:"gampaha",
+    email:"saman@gmil.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+},{
+    name:"nimal",
+    age:30,
+    address:"panadura",
+    email:"nimal@gmail.com"
+}];
 
-];
-
-customers.push({
-    name: "siripala",
-    age: 78,
-    address: "matugama"
-})
-
-customers.push({
-    name: "siripala",
-    age: 78,
-    address: "matugama"
-})
-
-
-function loadTable(){
-    let tblCustomerList =document.getElementById("tblCustomerList");
-    let body=`
-           <tr>
-            <th>Name</th>
-            <th>address</th>
-            <th>age</th>
-        </tr>`;
+// for(let i =0; i<customerList.length;i++){
+//     console.log(customerList[i]);
     
-    customers.forEach(customer =>{
-        body+=`
-        <tr>
+// }
+
+// for(let customer of customerList){
+//     console.log(customer);
+// }
+
+let tblCustomers = document.getElementById("tblCustomerList");
+
+let tblBody="";
+
+customerList.forEach(customer =>{
+    console.log(customer);
+    tblBody+=`
+            <tr>
             <td>${customer.name}</td>
             <td>${customer.address}</td>
             <td>${customer.age}</td>
-        </tr>`;
-    })
-    tblCustomerList.innerHTML=body;
-    console.log(body);
+            <td>${customer.email}</td>
+        </tr>
+    `;
     
+})
 
-}
-loadTable();
-function addCustomer() {
-    let custName = document.getElementById("txtName").value;
-    let custAddress = document.getElementById("txtAddress").value;
-    let custAge = document.getElementById("txtAge").value;
+tblCustomers.innerHTML=tblBody;
 
-    customers.push({
-        name:custName,
-        address:custAddress,
-        age:custAge
-    });
-    loadTable();
-    console.log(customers);
-}
+console.log(tblBody);
+
+
+//console.log(customerList[0]);
+
+
+
 
